2cc6436430557a4bf2ac0c7f61ec577067b5464f
DariaKarpovich <dashka230@yandex.ru>
2020-10-01 22:28:06 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+   if (array == [] || array == 0 || array == undefined) {
+        return 0;
+    } else {
+         let result = array[0];
+         for (let i = 1; i < array.length; i++) {
+            if (result > array[i]) result = array[i]; 
+         }
+    return result;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+if (array == [] || array == 0 || array == undefined) {
+        return 0;
+    } else {
+        let result = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (result < array[i]) result = array[i]; 
+        }
+    return result;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+ if (array == [] || array == 0 || array == undefined) {
+        return 0;
+    } else {
+        let result = 0;
+        for (let i = 0; i < array.length; i++) {
+            result += array[i];
+        }
+    return result / array.length;
+    }
 }
